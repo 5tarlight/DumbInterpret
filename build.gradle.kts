1048a6fd5151ae6ee6fb651c5331bb0a59ef8cbe
YEAHx4 <developerstarlight@gmail.com>
2024-03-25 12:57:00 +0900

    jar build gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.yeahx4"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -12,6 +12,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "io.yeahx4.Main"
+    }
 }
 
 tasks.test {
